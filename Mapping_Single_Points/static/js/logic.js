cd3003af6facce344a6488357756812125089e42
yazhcodes <yazhini.bakeerathan@gmail.com>
2021-05-20 17:18:29 -0400

    Marking a single city

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,4 +1,4 @@
-let mymap = L.map('mapid').setView([40.7, -94.5], 4);
+let mymap = L.map('mapid').setView([34.0522, -118.2437], 14);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', 
 {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,3 +6,9 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     accessToken: apiKey
 })
 streets.addTo(mymap);
+
+L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: 'black',
+    fillColor: 'yellow'
+}).addTo(mymap);
