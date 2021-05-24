f19f534934c7a2f76c73d1e329efc8e57ab0a661
yazhcodes <yazhini.bakeerathan@gmail.com>
2021-05-24 12:40:11 -0400

    Mapping GeoJSON Data with layer control

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,44 +1,32 @@
-// Declare variables
-let myMap = L.map('mapid').setView([37.61899948120117,-122.375], 10);
-let darkMap = 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}';
-let nightNavMap = 'https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v2/tiles/{z}/{x}/{y}?access_token={accessToken}';
-let outdoorMap = 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}';
+// Data
+let airportData = 'https://raw.githubusercontent.com/yazhcodes/Mapping_Earthquakes/Mapping_GeoJSON_Points/Resources/Data/majorAirports.json';
+
+// Declare map variables
+let streetUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}';
+let darkUrl = 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}';
 let tileLayerProps = {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: apiKey
 };
-let sanFranAirport = {
-    "type":"FeatureCollection",
-    "features":[{
-        "type":"Feature",
-        "properties": {
-            "id":"3469",
-            "name":"San Francisco International Airport",
-            "city":"San Francisco",
-            "country":"United States",
-            "faa":"SFO",
-            "icao":"KSFO",
-            "alt":"13",
-            "tz-offset":"-8",
-            "dst":"A",
-            "tz":"America/Los_Angeles"
-        },
-        "geometry": {
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]
-        }
-    }]
+let streetMap = L.tileLayer(streetUrl,tileLayerProps);
+let darkMap = L.tileLayer(darkUrl,tileLayerProps);
+let baseMaps = {
+    Street: streetMap,
+    Dark: darkMap
 };
+let myMap = L.map('mapid', {
+    center: [30, 30], 
+    zoom: 2,
+    layers: [streetMap]
+});
 
 // Plot the Map
-L.tileLayer(outdoorMap,tileLayerProps).addTo(myMap);
-L.geoJSON(sanFranAirport,{
-    // pointToLayer: function(feature,latlng) {
-    //     return L.marker(latlng)
-    //             .bindPopup(`<h2>${feature.properties.name}</h2><hr><h3>${feature.properties.city}, ${feature.properties.country}`);
-    onEachFeature: function(feature,layer) {
-        layer.bindPopup(`<h2>${feature.properties.faa}</h2><hr><h3>${feature.properties.name}`);
-    }    
+L.control.layers(baseMaps).addTo(myMap);
+d3.json(airportData).then(function(data){
+    L.geoJSON(data,{
+        onEachFeature: function(feature,layer){
+            layer.bindPopup(`<h2>${feature.properties.faa}</h2><hr><h3>${feature.properties.name}`);
+        }
+    }).addTo(myMap);
 })
-.addTo(myMap);
